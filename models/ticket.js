955817fc8d1b90f2a981508490b8a964bc3310a5
JohnB <jhondark@gmail.com>
2024-01-20 00:12:23 -0500

    Update username validation in ticket model

--- models/ticket.js
+++ models/ticket.js
@@ -4,7 +4,7 @@ const ticketSchema = new Schema(
   {
     username: {
       type: String,
-      required: [false, "username is required"],
+      required: [true, "username is required"],
       minLength: [3, "username must be at least 3 characters"],
       maxLength: [30, "username must be at most 30 characters"],
     },
